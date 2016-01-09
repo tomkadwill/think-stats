bcea126c42240da25cbf8925d48ceba8c9b0eeb8
Tom Kadwill <tomkadwill@gmail.com>
2016-01-09 22:47:40 +0000

    Trying to understand PMFs

--- chap2/RemainingLifetime.py
+++ chap2/RemainingLifetime.py
@@ -1,5 +1,8 @@
 import Pmf
 
+# Usage:
+# ages = [55, 63, 78, 64, 52, 88, 101, 11, 73]
+# RemainingLifetime.RemainingLifetime(12, ages).Prob(55)
 def RemainingLifetime(age, lifetimes):
     modified_lifetimes = []
     for lifetime in lifetimes:
@@ -8,3 +11,21 @@ def RemainingLifetime(age, lifetimes):
     pmf = Pmf.MakePmfFromList(modified_lifetimes)
     pmf.Normalize()
     return pmf
+
+# Question for Steven:
+
+# I think this is saying: given these are the recorded ages of a population
+# When I am 12 years old, what is probability of living until I am 55.
+# Is that correct?
+
+# Is this correct? Even though, when I put in the probability of living to
+# an age that is not there it returns 0.
+
+
+# For Tom:
+# This doesn't seem right. Create a new file (keep this) and try again.
+# The probability to living to 101 if you are 88 is 100%
+# RemainingLifetime.RemainingLifetime(88, ages).Prob(101)
+# but
+# The probability of living to 55 if you are 54 is only 14%
+# RemainingLifetime.RemainingLifetime(54, ages).Prob(55)
